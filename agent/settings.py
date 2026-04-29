8fc03b13e8abb2c31328e502405f7d95e3cf5297
Rohit10jr <rohitjworkspace@gmail.com>
2026-04-29 16:34:29 +0530

    add cors

--- agent/settings.py
+++ agent/settings.py
@@ -52,7 +52,7 @@ INSTALLED_APPS = [
 
     'core',
     "rest_framework",
-    # "corsheaders",
+    "corsheaders",
     # 'django_filters',
     # 'django_celery_results',
     # 'rest_framework.authtoken',
@@ -70,6 +70,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -77,6 +78,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
 ]
 
 ROOT_URLCONF = 'agent.urls'
